3e3b1d576cbebcea0dff5fd90311cdd6c3e63f0c
Keith Brewster <keith.brewster22@gmail.com>
2020-08-16 15:28:01 -0400

    add home page

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import MainLayout from 'src/layouts/MainLayout'
+import AffirmationsCell from 'src/components/AffirmationsCell'
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+      <AffirmationsCell />
+    </MainLayout>
+  )
+}
+
+export default HomePage
